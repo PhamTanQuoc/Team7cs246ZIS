fb471cafdf69a6b36c78531c462376f58848f730
Min-Le <lehuyhoang123789@gmail.com>
2024-03-06 19:01:35 +0700

    update

---
+++ LeHuyHoang_7592/ThucHanh1/ThucHanh1/src/thuchanh1/ThucHanh1.java
@@ -0,0 +1,12 @@
+
+package thuchanh1;
+
+public class ThucHanh1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormChinh frm = new FormChinh();
+        frm.setVisible(true);
+    }
+    
+}
