902ee85e75828835e1eb2de965ebc8b0d8be8f48
Min-Le <lehuyhoang123789@gmail.com>
2024-05-22 18:23:56 +0700

    update

---
+++ LeHuyHoang_7592/Đồ Án Cá Nhân/Bai_Ca_Nhan/src/thuchanh1/Thuc_Hanh1.java
@@ -0,0 +1,12 @@
+
+package thuchanh1;
+
+public class Thuc_Hanh1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormChinh frm = new FormChinh();
+        frm.setVisible(true);
+    }
+    
+}
